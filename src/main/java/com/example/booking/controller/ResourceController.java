fd76e30a58be378d6bbfa7d831fb79230231134a
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-07-01 15:14:36 +0300

    save details

--- src/main/java/com/example/booking/controller/ResourceController.java
+++ src/main/java/com/example/booking/controller/ResourceController.java
@@ -4,6 +4,7 @@ import com.example.booking.model.Resource;
 import com.example.booking.service.ResourceService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
